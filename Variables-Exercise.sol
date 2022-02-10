8adcd6495a0012d553154aca7e3296e5bbd0aaef
Mars Ifeanyi <Marschalice@gmail.com>
2022-02-10 23:02:29 +0000

    Declaring and assigning variable in solidity

---
+++ Variables-Exercise.sol
@@ -0,0 +1,14 @@
+// Exercise 1. create a new variable called wallet as an integer
+// 2. create a boolean called spend
+// 3. create a string give it the name notifySpend
+//4. initialize the wallet to 500
+//5. set the value of spend to false
+//6. add the string literal (the string value ) 'you have spent money' to notifySpend
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract variable {
+    uint256 wallet = 500;
+    bool spend = false;
+    string notifySpend = "you have spent money";
+}
