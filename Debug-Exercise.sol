f88a4fd204b75bff525eacbe0cb3db8b3b12788d
Mars Ifeanyi <Marschalice@gmail.com>
2022-02-11 10:05:44 +0000

    Smart contract Arduiting,debugging error codes

---
+++ Debug-Exercise.sol
@@ -0,0 +1,54 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+/* 
+
+You just land your new blockchain job to build smart contracts for an awesome DAO catching fire!
+
+Immediately, your boss throws at your a scrip of code tons of bugs in it. She says, 
+
+'Hey you! Fix this code and get this smart contract running and help save our DApp from crashing!'
+
+The clock is ticking and this is the perfect opportunity to prove your skills and show what you are made of.
+
+Your assignment is to fix the code below full of syntax errors and get this contract successfully deployed on 
+the Ethereum network. 
+
+Good luck! 
+
+*/
+
+contract learnFunctions{
+    
+   
+   functon remoteControlOpen (bool closedDoor, bool openDoor ) public returns(bool) {}
+       
+       uint = 45;
+       uint = a; 
+
+   function addfValues() public view returns(uint) {
+       uint b = 3;
+       uint result = a + b;
+       return result;
+   }
+
+   function addNewValues() public view returns (string memory) {
+       uint b = 5;
+       uint result = a + b;
+       return result;
+   }
+       
+   }
+   
+   uint += 4;
+   
+    function multiplyCalculatorByFour(uint a, uint b) public view returns (uint) {
+        uint result = a * b;
+        return result;
+    }
+    
+    function divideCalculatorByFour(uint a ) public view returns  (uint) {
+        a / b = uint result;
+        return result;
+    }
+   
+}
