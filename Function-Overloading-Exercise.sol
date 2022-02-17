a8b97dcf0ee9abdac67ae9002b56bac14f4abc60
Mars Ifeanyi <Marschalice@gmail.com>
2022-02-17 11:54:16 +0000

    Write function-overloading exercise

--- Function-Overloading-Exercise.sol
+++ Function-Overloading-Exercise.sol
@@ -1,5 +1,4 @@
-/*
-Exercise: 
+/*            Exercise on Function Overloading 
 1. Create two functions with the same name that return the sum of their arguments - 
 2. One function which takes two arguments and another function which takes three arguments. 
 3. Create two additional functions which can call each sum function and return their various sums. 
@@ -11,8 +10,8 @@ Exercise:
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Overloading {
-    /* Function overloading = Function wirh the same name but with different number of arguments*/
-    function mySum(uint256 a, uint256 b) public view returns (uint256) {
+    /* Function overloading = Function with the same name but with different number of arguments*/
+    function mySum(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
 
@@ -20,15 +19,15 @@ contract Overloading {
         uint256 a,
         uint256 b,
         uint256 c
-    ) public view returns (uint256) {
+    ) public pure returns (uint256) {
         return a + b + c;
     }
 
-    function getMySum() public view returns (uint256) {
-        return (20, 40);
+    function getMySum2() public pure returns (uint256) {
+        return mySum(20, 40);
     }
 
-    function getMySum() public view returns (uint256) {
-        return (100, 50, 30);
+    function getMySum3() public pure returns (uint256) {
+        return mySum(100, 50, 30);
     }
 }
