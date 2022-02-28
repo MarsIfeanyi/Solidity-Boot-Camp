d6539477e03d849c49595e71892167fee2bbaece
Mars Ifeanyi <Marschalice@gmail.com>
2022-02-28 10:51:49 +0000

    Write OnlyBy Restricted access
    I wrote OnlyBy Restricted access pattern which only allows the current caller (Mentioned Caller) to change the address ie only the owner can change the address of the contract

---
+++ RestrictedAccess Pattern/OnlyBy.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract RestrictedAcces {
+    //initializing the state variables to the current caller and timestamp
+    address public owner = msg.sender;
+    uint256 public creationTime = block.timestamp;
+
+    /*write a modifier called onlyBy that will require the current caller to be equal to an account address which we set as an input in the modifier and if the requirement fails send a message that says - Sender not authorized!*/
+
+    //MyHint: onlyBy, means that only the mentioned caller can call this function.
+    modifier onlyBy(address _account) {
+        require(msg.sender == _account, "Sender not authorized!");
+        _;
+    }
+
+    /* write a function that will change the owner address.
+ MyHint: Owner was our state variable address, now change it to a new address using a function */
+    function changeOwnerAddress(address _newAddress) public onlyBy(owner) {
+        owner = _newAddress;
+    }
+}
