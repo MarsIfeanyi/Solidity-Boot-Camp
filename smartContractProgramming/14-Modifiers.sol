8c85d8231d4792a57f138e85193fd26ecea843b4
MarsIfeanyi <marschalice@gmail.com>
2022-12-13 23:45:08 +0100

    Initial Commit

---
+++ smartContractProgramming/14-Modifiers.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * Function modifiers allows you to reuse code.
+ */
+
+contract FunctionModifier {
+    bool public paused;
+    uint public count;
+
+    function setPause(bool _paused) external {
+        paused = _paused;
+    }
+
+    //Basic modifiers
+    modifier whenNotPaused() {
+        require(!paused, "paused");
+        _;
+    }
+
+    function inc() external whenNotPaused {
+        count += 1;
+    }
+
+    function dec() external whenNotPaused {
+        count -= 1;
+    }
+
+    //Modifiers that takes in an input
+    modifier cap(uint _x) {
+        require(_x < 100, "x >=100");
+        _;
+    }
+
+    function incBy(uint _x) external whenNotPaused cap(_x) {
+        count += _x;
+    }
+
+    //Sandwich modifier... Some code will be executed inside the function modifier, call the actual function and then execute more code inside the function modifier
+
+    modifier sandwich() {
+        //code here
+        count += 10;
+        _;
+
+        // more code here
+        count *= 2;
+    }
+
+    function sandwichInBy() external sandwich {
+        count += 1;
+    }
+}
