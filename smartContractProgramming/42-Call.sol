aa4c8868b8861c2d791af979700ba3359141fefb
MarsIfeanyi <marschalice@gmail.com>
2022-12-20 22:03:52 +0100

    Update file

---
+++ smartContractProgramming/42-Call.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * CALL
+ *
+ * Call is a low level function that we can use to send ether to another contract.
+ *
+ *
+ */
+
+contract TestCall {
+    string public message;
+    uint public x;
+
+    event Log(string message);
+
+    fallback() external payable {
+        emit Log("fallback was called");
+    }
+
+    function foo(
+        string memory _message,
+        uint _x
+    ) external payable returns (bool, uint) {
+        message = _message;
+        x = _x;
+        return (true, 999);
+    }
+}
+
+contract Call {
+    bytes public data;
+
+    function callFoo(address _test) external payable {
+        (bool success, bytes memory _data) = _test.call{value: 111}(
+            abi.encodeWithSignature("foo(string, uint256) ", "call foo", 123)
+        );
+        require(success, "call failed");
+        data = _data;
+    }
+
+    function callDoesNotExit(address _test) external {
+        (bool success, ) = _test.call(
+            abi.encodeWithSignature("doesNotExist() ")
+        );
+
+        require(success, "call failed");
+    }
+}
