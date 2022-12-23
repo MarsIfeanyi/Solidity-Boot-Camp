5f431296b53216a9c6e340397b19571ee8e3f4e1
MarsIfeanyi <marschalice@gmail.com>
2022-12-23 10:54:36 +0100

    Initial Commit

---
+++ Growic/week3/Struct.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * You can define your own type by creating a struct.
+They are useful for grouping together related data.
+Structs can be declared outside of a contract and imported into another contract.
+ */
+
+contract Struct {
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    // An array of 'Todo' structs
+    Todo[] public todos;
+
+    function create(string calldata _text) public {
+        // 3 ways to initialize a struct
+        // 1.  calling it like a function
+        todos.push(Todo(_text, false)); //Hint: Here the order is very important
+
+        // 2. key value mapping
+        todos.push(Todo({text: _text, completed: false}));
+
+        // 3. initialize an empty struct and then update it
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed initialized to false
+
+        todos.push(todo);
+    }
+
+    // Solidity automatically created a getter for 'todos' so
+    // you don't actually need this function.
+    function get(
+        uint _index
+    ) public view returns (string memory text, bool completed) {
+        Todo storage todo = todos[_index];
+        return (todo.text, todo.completed);
+    }
+
+    // update text
+    function updateText(uint _index, string calldata _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+
+    // update completed
+    function toggleCompleted(uint _index) public {
+        Todo storage todo = todos[_index];
+        todo.completed = !todo.completed;
+    }
+}
