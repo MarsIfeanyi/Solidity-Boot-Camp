4af442bb2149087ebdca414eb0b0e5e1d328532a
MarsIfeanyi <marschalice@gmail.com>
2023-01-10 21:05:35 +0100

    Update file

--- smartContractProgramming/55-EnglishAuction.sol
+++ smartContractProgramming/55-EnglishAuction.sol
@@ -3,20 +3,43 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /**
- * In English Auction, the seller sets starting price and ending time and then at the end of the auction, the highest bidder wins
+ * In English Auction, the seller sets starting price and ending time and then at the end of the auction, the highest bidder wins.
+ * 
+ * 
+ * English auction for NFT.
+
+Auction
+- Seller of NFT deploys this contract.
+- Auction lasts for 7 days.
+- Participants can bid by depositing ETH greater than the current highest bidder.
+- All bidders can withdraw their bid if it is not the current highest bid.
+
+After the auction
+- Highest bidder becomes the new owner of NFT.
+- The seller receives the highest bid of ETH.
  */
 
 interface IERC721 {
-    function transferFrom(address from, address to, uint nftId) external;
+    function safeTransferFrom(address from, address to, uint tokenId) external;
+
+    function transferFrom(address, address, uint) external;
+
+    /**
+     * Hint: 
+     * transferFrom() is a basic function that allows tokens to be transferred from one address to another without any additional safety checks.
+     
+     safeTransferFrom() includes additional safety checks to ensure the transfer can be completed successfully and make sure the recipient is able to handle the tokens.
+     */
 }
 
 contract EnglishAuction {
-    // event
-
+    // events
     event Start();
     event Bid(address indexed sender, uint amount);
     event Withdraw(address indexed bidder, uint amount);
+    event End(address highestBidder, uint amount);
 
+    // state variables
     IERC721 public immutable nft;
     uint public immutable nftId;
 
@@ -25,25 +48,28 @@ contract EnglishAuction {
 
     // keeping track of the state of the auction
     bool public started; // sets to true when the auction starts
-    bool public eneded; // sets to true when the auction ends
+    bool public ended; // sets to true when the auction ends
 
     address public highestBidder;
-    uint public highestBid;
+    uint public highestBid; // this is the amount
     mapping(address => uint) public bids;
 
     constructor(address _nft, uint _nftId, uint _startingBid) {
+        // initializing the state variables
+
         nft = IERC721(_nft); // _nft from the input
         nftId = _nftId;
         seller = payable(msg.sender);
         highestBid = _startingBid;
     }
 
+    // function to start the auction, this function will be called outside the contract
     function start() external {
         require(msg.sender == seller, "not seller");
         require(!started, "started");
 
         started = true;
-        endAt = uint32(block.timestamp + 60); // block.timestamp is uint256, thus we are converting it to uint32
+        endAt = uint32(block.timestamp + 7 days); // block.timestamp is uint256, thus we are converting it to uint32
 
         // transferring the ownership of the nft from the seller to this contract, so that this contract will own the nft
         nft.transferFrom(seller, address(this), nftId);
@@ -57,7 +83,7 @@ contract EnglishAuction {
         require(msg.value > highestBid, "value is less than highest bid");
 
         if (highestBidder != address(0)) {
-            bids[highestBidder] += highestBid;
+            bids[highestBidder] += highestBid; // bids[address] += amount;
         }
         highestBid = msg.value;
         highestBidder = msg.sender;
@@ -75,5 +101,27 @@ contract EnglishAuction {
         payable(msg.sender).transfer(bal);
 
         emit Withdraw(msg.sender, bal);
+    }
+
+    function end() external {
+        require(started, "not started");
+        require(!ended, "ended");
+        require(block.timestamp >= endAt, "not ended");
+
+        ended = true;
+
+        // if there is a highestBidder then:
+        if (highestBidder != address(0)) {
+            // transfer the  ownership of the nft from this contract to the  highestBidder
+            nft.transferFrom(address(this), highestBidder, nftId);
+            //transfer highestBid to the seller
+            seller.transfer(highestBid);
+        }
+        // if there is no highestBidder, transfer the ownership of the nft back to the seller
+        else {
+            nft.safeTransferFrom(address(this), seller, nftId);
+        }
+
+        emit End(highestBidder, highestBid);
     }
 }
