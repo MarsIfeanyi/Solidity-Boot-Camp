eb50ceaf972c3f5fc7a8e385053286c30319a052
MarsIfeanyi <marschalice@gmail.com>
2023-08-15 22:01:27 +0100

    Update file

--- smartContractProgramming/14-Modifiers.sol
+++ smartContractProgramming/14-Modifiers.sol
@@ -16,7 +16,9 @@ contract FunctionModifier {
     //Basic modifiers
     modifier whenNotPaused() {
         require(!paused, "paused");
-        _; // Hint: The underscore tells solidity to call the actual function that this modifier wraps
+        _; // Hint: The underscore tells solidity to call the actual function that this modifier wraps... ie we run the rest of the function that called this modifier(funtion where this modifier is added).
+
+        // VIP:  A single modifier can be used by any number of functions.
     }
 
     function inc() external whenNotPaused {
@@ -50,5 +52,25 @@ contract FunctionModifier {
 
     function sandwichInBy() external sandwich {
         count += 1;
+    }
+
+    /**
+     * Modifiers can take inputs too - you’d just pass the type and name of the input into a modifier.
+     *
+     * Modifiers are a great way to package up snippets of logic that can be reused in various smart contracts that together power your dApp. Reusing logic makes your code easier to read, maintain and reason about – hence the principle DRY (Don’t Repeat Yourself).
+     */
+
+    modifier validAddress(address addr) {
+        // address should not be a zero-address.
+        require(addr != address(0), "Address invalid");
+
+        // continue with the rest of the logic
+        _;
+    }
+
+    function transferTokenTo(
+        address someAddress
+    ) public validAddress(someAddress) {
+        // do something....
     }
 }
